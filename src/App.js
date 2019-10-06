e9d5068c8611e4d34c2d640c4a0e832bf2a236b3
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2019-10-06 10:52:31 -0300

    Implementando redux saga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+
 import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
@@ -16,6 +18,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyle />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   );
